c8f0f091cd8468bb370978b6ed793675f8f669c0
SyedOmar <syed.omar@ucalgary.ca>
2025-07-28 16:00:29 -0600

    Setting up rock paper scissors game and adding more plans in Planning.txt

--- Main.js
+++ Main.js
@@ -1,1 +1,18 @@
-alert("hello this is a text")
+alert("Welcome! Please choose either Rock, Paper or Scissors.");
+
+let num = Math.random()
+console.log(num);
+
+let randNum = Math.floor(num * 3);
+console.log(randNum);
+
+let oppChoice;
+
+if (randNum == 0){
+    oppChoice = "Rock"
+}else if (randNum == 1){
+    oppChoice = "Paper"
+}else{
+    oppChoice = "Scissors"
+}
+console.log(oppChoice);
